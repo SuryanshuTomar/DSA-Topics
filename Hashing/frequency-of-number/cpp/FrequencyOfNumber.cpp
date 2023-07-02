d81a0267044928c19617aca43a45ecdcbb86f29b
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-07-02 10:12:39 +0530

    add: function for printing 1d vector

--- Hashing/frequency-of-number/cpp/FrequencyOfNumber.cpp
+++ Hashing/frequency-of-number/cpp/FrequencyOfNumber.cpp
@@ -1,2 +1,35 @@
 // https://www.geeksforgeeks.org/find-frequency-number-array/
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void findFrequencyNumber(vector<int> &freqArr, int size, int queries) {
+   cout << "No. of queries you can ask : " << queries << endl;   
+   for(int i = 0; i < queries; i++){
+      int query;
+      cout << "Query " << i + 1 << " ";
+      cin >>  query;
+      cout <<  "Result : " << freqArr[query] << endl;
+   }
+}
+
+void calculateFreqArr(vector<int> &arr, vector<int> &freqArr, int size) {
+  for (int i = 0; i < size; i++) {
+      freqArr[arr[i]]++;
+  }
+}
+
+int main() {
+  vector<int> arr = {1, 1, 1, 3, 4, 5, 7, 8, 9, 9, 9, 9, 2, 2};
+
+  int queries = 3;
+  int size = arr.size();
+
+  vector<int> freqArr{size};
+
+  cout << "Size of frequency arr : " << size << endl;
+  calculateFreqArr(arr, freqArr, size);
+  findFrequencyNumber(freqArr, size, queries);
+  return 0;
+}
