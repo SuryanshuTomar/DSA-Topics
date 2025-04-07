b20096dfa323a7882c278075815f7bc848340552
Suryanshu Tomar <suryanshutomar123@outlook.com>
2025-04-07 20:34:30 +0530

    refactor : added notes

--- Hashing/frequency-of-number/cpp/FrequencyOfNumber.cpp
+++ Hashing/frequency-of-number/cpp/FrequencyOfNumber.cpp
@@ -1,24 +1,24 @@
 // https://www.geeksforgeeks.org/find-frequency-number-array/
 
+#include "../../helper-functions/PrintArrCpp.h"
 #include <iostream>
 #include <vector>
-#include "../../helper-functions/PrintArrCpp.h"
 
 using namespace std;
 
 void findFrequencyNumber(vector<int> &freqArr, int size, int queries) {
-   cout << "No. of queries you can ask : " << queries << endl;   
-   for(int i = 0; i < queries; i++){
-      int query;
-      cout << "Query " << i + 1 << " -> ";
-      cin >>  query;
-      cout <<  "Result : " << freqArr[query] << endl;
-   }
+  cout << "No. of queries you can ask : " << queries << endl;
+  for (int i = 0; i < queries; i++) {
+    int query;
+    cout << "Query " << i + 1 << " -> ";
+    cin >> query;
+    cout << "Result : " << freqArr[query] << endl;
+  }
 }
 
 void calculateFreqArr(vector<int> &arr, vector<int> &freqArr, int size) {
   for (int i = 0; i < size; i++) {
-      freqArr[arr[i]]++;
+    freqArr[arr[i]]++;
   }
 }
 
@@ -31,13 +31,32 @@ int main() {
   vector<int> freqArr(100);
 
   cout << "Size of frequency arr : " << size << endl;
+
+  // Note:
+  //  1. We can only declare 10^6 size of int array inside main and 10^7 size of
+  //  int array outside of the main.
+  //  2. For bool array it is 10^7 size inside main and 10^8 size outside of
+  //  main.
+
+  // precompute
+  // If the problem statement states that the size of the array will be 10^5
+  // then we will take the size of the hash array to 10^5 + 1.
+  // like this -> int hash[100001]
+
+  // precompute
+  // If the problem statement states that the size of the array will be 10^5
+  // then we will take the size of the hash array to 10^5 + 1.
+  // like this -> int hash[100001];
   calculateFreqArr(arr, freqArr, size);
+
   printVector(freqArr);
+
   findFrequencyNumber(freqArr, size, queries);
   return 0;
 }
 
-// Time Complexity : O(n + q), where n is the size of the array and the q is the number of queries
+// Time Complexity : O(n + q), where n is the size of the array and the q is the
+// number of queries
 //  Space Complexity : (n), n is the size of the extra space
 // taken by the frequency array. Also, the size of the frequency array depends
 // on the max number present in the input array.
